556a21468facc2c7eaa0e348d1ec52a704917215
LIMYUNJI <yeinlim12@naver.com>
2021-02-14 02:51:04 +0900

    2021_02_14_모델 수정

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -1,11 +1,12 @@
 <?php
-namespace App;
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    protected $table = 'member';
-    public $timestamps = false;
+    use HasFactory;
 }
 ?>
